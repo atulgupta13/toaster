22c68a7ca08f3cd4a5fa9be6183bee06293f075c
atulgupta <atul.gupta@techchefz.com>
2020-07-25 15:51:34 +0530

    DeviceID toast added

--- mysdk/src/main/java/com/example/mysdk/FetchData.java
+++ mysdk/src/main/java/com/example/mysdk/FetchData.java
@@ -3,16 +3,18 @@ package com.example.mysdk;
 
 import android.annotation.SuppressLint;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.provider.Settings;
 import android.util.Log;
+import android.widget.Toast;
 
 public class FetchData{
 
-    public static void main(){
+    public static void s(Context c, String message){
 
         Object id = getContentReseiver();
         Log.i("obj",id.toString());
-
+        Toast.makeText(c, id.toString(), Toast.LENGTH_SHORT).show();
     }
 
     public static Object getContentReseiver() {
